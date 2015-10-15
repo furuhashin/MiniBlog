68f4fc626eef4b5c9d56dbc222a890f31ed90fa1
nob <furuhashinobukatsu@furuhashinobukatsu-no-MacBook-Pro.local>
2015-10-16 01:43:47 +0900

    layout.phpの作成

---
+++ views/layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php if (isset($title)) : echo $this->escape($title) . ' - ' ; 
+	endif ; ?>MIni Blog</title>
+</head>
+<body>
+	<div id = "header">
+		<h1><a href="<?php echo $base_url; ?>/">Mini Blog</a></h1>
+	</div>
+
+	<div id = "main">
+		<?php echo $_content; ?>
+	</div>
+
+</body>
+</html>
