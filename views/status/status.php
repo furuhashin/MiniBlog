6d96e14cae523efd4e574fb51f13c74960cb1a65
nob <furuhashinobukatsu@furuhashinobukatsu-no-MacBook-Pro.local>
2015-12-07 11:52:51 +0900

    2015/12/07 レイアウトの修正

--- views/status/status.php
+++ views/status/status.php
@@ -3,7 +3,9 @@
             <a href = "<?php echo $base_url; ?>/user/<?php echo $this->escape($status['user_name']); ?>">
             <?php echo $this->escape($status['user_name']); ?>
             </a>
+            <BR>
             <?php echo $this->escape($status['body']); ?>
+            </BR>
         </div>
         <div>
             <a href="<?php echo $base_url; ?>/user/<?php echo $this->escape($status['user_name']); ?>
